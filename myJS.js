73e52282bae1b681b0e5242cf3f6e1d9f0bbf049
Ulf Bengtsson <Ulf.Bengtsson.Lexicon@outlook.com>
2021-04-08 10:06:00 +0200

    first commit

---
+++ myJS.js
@@ -0,0 +1,34 @@
+const arrayOfBoxes = [
+    [["red"],["blue"],["green"]],
+    [["blue"],["green"],["red"]],
+    [["green"],["red"],["blue"]]
+];
+const myBoard = document.getElementById("board");
+var greenBoxes = [];
+
+function makeBox(color, y, x) {
+    let newBox = document.createElement("div");
+
+    newBox.id = "y" + y + "x" + x;
+    newBox.classList.add("box");
+    newBox.classList.add(color + "Box");
+    myBoard.appendChild(newBox);
+
+    if(color == "green") {
+        greenBoxes.push(newBox);
+    }
+}
+
+for (let y = 0; y < arrayOfBoxes.length; y++) {
+
+    //console.log(arrayOfBoxes[y]);
+
+    for (let x = 0; x < arrayOfBoxes[y].length; x++) {
+
+        //console.log(arrayOfBoxes[y][x][0]);
+        makeBox(arrayOfBoxes[y][x], y, x);
+    }
+    
+}
+
+console.log(greenBoxes);
